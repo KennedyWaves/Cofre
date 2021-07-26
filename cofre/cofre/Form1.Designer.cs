ee491cd86e97b02c5588f2be96543a7aceea469a
Kennedy Souza <kennedy.es.souza@gmail.com>
2021-07-26 15:56:41 -0300

    Initial Efforts

---
+++ cofre/cofre/Form1.Designer.cs
@@ -0,0 +1,415 @@
+﻿namespace cofre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_add1 = new System.Windows.Forms.TextBox();
+            this.txt_t1 = new System.Windows.Forms.TextBox();
+            this.txt_t050 = new System.Windows.Forms.TextBox();
+            this.txt_add050 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_qt050 = new System.Windows.Forms.TextBox();
+            this.txt_t025 = new System.Windows.Forms.TextBox();
+            this.txt_add025 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_qt025 = new System.Windows.Forms.TextBox();
+            this.txt_t005 = new System.Windows.Forms.TextBox();
+            this.txt_add005 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_qt005 = new System.Windows.Forms.TextBox();
+            this.txt_t010 = new System.Windows.Forms.TextBox();
+            this.txt_add010 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_qt010 = new System.Windows.Forms.TextBox();
+            this.txt_totalAbs = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txt_tcoins = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txt_qt1 = new System.Windows.Forms.TextBox();
+            this.cmd_save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Moedas:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Add. moedas:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "R$ 1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(161, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Quantidade:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(241, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Total:";
+            // 
+            // txt_add1
+            // 
+            this.txt_add1.Location = new System.Drawing.Point(90, 31);
+            this.txt_add1.Name = "txt_add1";
+            this.txt_add1.Size = new System.Drawing.Size(63, 20);
+            this.txt_add1.TabIndex = 9;
+            this.txt_add1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_add1_KeyDown);
+            // 
+            // txt_t1
+            // 
+            this.txt_t1.Location = new System.Drawing.Point(232, 31);
+            this.txt_t1.Name = "txt_t1";
+            this.txt_t1.ReadOnly = true;
+            this.txt_t1.Size = new System.Drawing.Size(63, 20);
+            this.txt_t1.TabIndex = 10;
+            this.txt_t1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt_t050
+            // 
+            this.txt_t050.Location = new System.Drawing.Point(232, 57);
+            this.txt_t050.Name = "txt_t050";
+            this.txt_t050.ReadOnly = true;
+            this.txt_t050.Size = new System.Drawing.Size(63, 20);
+            this.txt_t050.TabIndex = 14;
+            this.txt_t050.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt_add050
+            // 
+            this.txt_add050.Location = new System.Drawing.Point(90, 57);
+            this.txt_add050.Name = "txt_add050";
+            this.txt_add050.Size = new System.Drawing.Size(63, 20);
+            this.txt_add050.TabIndex = 13;
+            this.txt_add050.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_add050_KeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "R$ 0,50";
+            // 
+            // txt_qt050
+            // 
+            this.txt_qt050.Location = new System.Drawing.Point(161, 57);
+            this.txt_qt050.Name = "txt_qt050";
+            this.txt_qt050.ReadOnly = true;
+            this.txt_qt050.Size = new System.Drawing.Size(63, 20);
+            this.txt_qt050.TabIndex = 11;
+            this.txt_qt050.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt_t025
+            // 
+            this.txt_t025.Location = new System.Drawing.Point(232, 84);
+            this.txt_t025.Name = "txt_t025";
+            this.txt_t025.ReadOnly = true;
+            this.txt_t025.Size = new System.Drawing.Size(63, 20);
+            this.txt_t025.TabIndex = 18;
+            this.txt_t025.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt_add025
+            // 
+            this.txt_add025.Location = new System.Drawing.Point(90, 84);
+            this.txt_add025.Name = "txt_add025";
+            this.txt_add025.Size = new System.Drawing.Size(63, 20);
+            this.txt_add025.TabIndex = 17;
+            this.txt_add025.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_add025_KeyDown);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 87);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "R$ 0,25";
+            // 
+            // txt_qt025
+            // 
+            this.txt_qt025.Location = new System.Drawing.Point(161, 84);
+            this.txt_qt025.Name = "txt_qt025";
+            this.txt_qt025.ReadOnly = true;
+            this.txt_qt025.Size = new System.Drawing.Size(63, 20);
+            this.txt_qt025.TabIndex = 15;
+            this.txt_qt025.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt_t005
+            // 
+            this.txt_t005.Location = new System.Drawing.Point(232, 138);
+            this.txt_t005.Name = "txt_t005";
+            this.txt_t005.ReadOnly = true;
+            this.txt_t005.Size = new System.Drawing.Size(63, 20);
+            this.txt_t005.TabIndex = 26;
+            this.txt_t005.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt_add005
+            // 
+            this.txt_add005.Location = new System.Drawing.Point(90, 138);
+            this.txt_add005.Name = "txt_add005";
+            this.txt_add005.Size = new System.Drawing.Size(63, 20);
+            this.txt_add005.TabIndex = 25;
+            this.txt_add005.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_add005_KeyDown);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 141);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "R$ 0,05";
+            // 
+            // txt_qt005
+            // 
+            this.txt_qt005.Location = new System.Drawing.Point(161, 138);
+            this.txt_qt005.Name = "txt_qt005";
+            this.txt_qt005.ReadOnly = true;
+            this.txt_qt005.Size = new System.Drawing.Size(63, 20);
+            this.txt_qt005.TabIndex = 23;
+            this.txt_qt005.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt_t010
+            // 
+            this.txt_t010.Location = new System.Drawing.Point(232, 111);
+            this.txt_t010.Name = "txt_t010";
+            this.txt_t010.ReadOnly = true;
+            this.txt_t010.Size = new System.Drawing.Size(63, 20);
+            this.txt_t010.TabIndex = 22;
+            this.txt_t010.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt_add010
+            // 
+            this.txt_add010.Location = new System.Drawing.Point(90, 111);
+            this.txt_add010.Name = "txt_add010";
+            this.txt_add010.Size = new System.Drawing.Size(63, 20);
+            this.txt_add010.TabIndex = 21;
+            this.txt_add010.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_add010_KeyDown);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 114);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "R$ 0,10";
+            // 
+            // txt_qt010
+            // 
+            this.txt_qt010.Location = new System.Drawing.Point(161, 111);
+            this.txt_qt010.Name = "txt_qt010";
+            this.txt_qt010.ReadOnly = true;
+            this.txt_qt010.Size = new System.Drawing.Size(63, 20);
+            this.txt_qt010.TabIndex = 19;
+            this.txt_qt010.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt_totalAbs
+            // 
+            this.txt_totalAbs.Location = new System.Drawing.Point(232, 189);
+            this.txt_totalAbs.Name = "txt_totalAbs";
+            this.txt_totalAbs.ReadOnly = true;
+            this.txt_totalAbs.Size = new System.Drawing.Size(63, 20);
+            this.txt_totalAbs.TabIndex = 30;
+            this.txt_totalAbs.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(241, 171);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(48, 16);
+            this.label10.TabIndex = 29;
+            this.label10.Text = "Total:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(161, 173);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(65, 13);
+            this.label11.TabIndex = 28;
+            this.label11.Text = "Quantidade:";
+            // 
+            // txt_tcoins
+            // 
+            this.txt_tcoins.Location = new System.Drawing.Point(161, 189);
+            this.txt_tcoins.Name = "txt_tcoins";
+            this.txt_tcoins.ReadOnly = true;
+            this.txt_tcoins.Size = new System.Drawing.Size(63, 20);
+            this.txt_tcoins.TabIndex = 27;
+            this.txt_tcoins.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::cofre.Properties.Resources.wav;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 194);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 22);
+            this.pictureBox1.TabIndex = 31;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txt_qt1
+            // 
+            this.txt_qt1.Location = new System.Drawing.Point(161, 31);
+            this.txt_qt1.Name = "txt_qt1";
+            this.txt_qt1.ReadOnly = true;
+            this.txt_qt1.Size = new System.Drawing.Size(63, 20);
+            this.txt_qt1.TabIndex = 32;
+            this.txt_qt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // cmd_save
+            // 
+            this.cmd_save.Location = new System.Drawing.Point(161, 216);
+            this.cmd_save.Name = "cmd_save";
+            this.cmd_save.Size = new System.Drawing.Size(136, 23);
+            this.cmd_save.TabIndex = 33;
+            this.cmd_save.Text = "Salvar";
+            this.cmd_save.UseVisualStyleBackColor = true;
+            this.cmd_save.Click += new System.EventHandler(this.cmd_save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 251);
+            this.Controls.Add(this.cmd_save);
+            this.Controls.Add(this.txt_qt1);
+            this.Controls.Add(this.txt_add1);
+            this.Controls.Add(this.txt_add050);
+            this.Controls.Add(this.txt_add025);
+            this.Controls.Add(this.txt_add010);
+            this.Controls.Add(this.txt_add005);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txt_totalAbs);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txt_tcoins);
+            this.Controls.Add(this.txt_t005);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt_qt005);
+            this.Controls.Add(this.txt_t010);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txt_qt010);
+            this.Controls.Add(this.txt_t025);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_qt025);
+            this.Controls.Add(this.txt_t050);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_qt050);
+            this.Controls.Add(this.txt_t1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "COFRINHO";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_add1;
+        private System.Windows.Forms.TextBox txt_t1;
+        private System.Windows.Forms.TextBox txt_t050;
+        private System.Windows.Forms.TextBox txt_add050;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_qt050;
+        private System.Windows.Forms.TextBox txt_t025;
+        private System.Windows.Forms.TextBox txt_add025;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_qt025;
+        private System.Windows.Forms.TextBox txt_t005;
+        private System.Windows.Forms.TextBox txt_add005;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_qt005;
+        private System.Windows.Forms.TextBox txt_t010;
+        private System.Windows.Forms.TextBox txt_add010;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt_qt010;
+        private System.Windows.Forms.TextBox txt_totalAbs;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txt_tcoins;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txt_qt1;
+        private System.Windows.Forms.Button cmd_save;
+    }
+}
+
---
+++ cofre/cofre/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace cofre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int qt1 = 0;
+        int qt050 = 0;
+        int qt025 = 0;
+        int qt010 = 0;
+        int qt005 = 0;
+        private void txt_add1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    qt1 += Convert.ToInt32(txt_add1.Text);
+                    txt_qt1.Text = qt1.ToString();
+                    txt_t1.Text = "R$ " + qt1.ToString() + ",00";
+                    txt_add1.Text = "";
+                    update_total();
+                }
+                catch { }
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                txt_add050.Focus();
+            }
+        }
+
+        private void txt_add050_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    qt050 += Convert.ToInt32(txt_add050.Text);
+                    txt_qt050.Text = qt050.ToString();
+                    txt_t050.Text = "R$ " + String.Format("{0:0.00}",Math.Round((qt050 * 0.5), 2));
+                    txt_add050.Text = "";
+                    update_total();
+                }
+                catch { }
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                txt_add1.Focus();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                txt_add025.Focus();
+            }
+        }
+
+        void update_total()
+        {
+            double total = qt1 + (qt050 * 0.5) + (qt025 * 0.25) + (qt010 * 0.1) + (qt005 * 0.05);
+            double tcoins = qt1 + qt050 + qt025 + qt010 + qt005;
+            txt_tcoins.Text = tcoins.ToString();
+            txt_totalAbs.Text = "R$ " + String.Format("{0:0.00}", total);
+        }
+
+        private void txt_add025_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    qt025 += Convert.ToInt32(txt_add025.Text);
+                    txt_qt025.Text = qt025.ToString();
+                    txt_t025.Text = "R$ " + String.Format("{0:0.00}", Math.Round((qt025 * 0.25), 2));
+                    txt_add025.Text = "";
+                    update_total();
+                }
+                catch { }
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                txt_add050.Focus();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                txt_add010.Focus();
+            }
+        }
+
+        private void txt_add010_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    qt010 += Convert.ToInt32(txt_add010.Text);
+                    txt_qt010.Text = qt010.ToString();
+                    txt_t010.Text = "R$ " + String.Format("{0:0.00}", Math.Round((qt010 * 0.10), 2));
+                    txt_add010.Text = "";
+                    update_total();
+                }
+                catch { }
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                txt_add025.Focus();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                txt_add005.Focus();
+            }
+        }
+
+        private void txt_add005_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    qt005 += Convert.ToInt32(txt_add005.Text);
+                    txt_qt005.Text = qt005.ToString();
+                    txt_t005.Text = "R$ " + String.Format("{0:0.00}", Math.Round((qt005 * 0.05), 2));
+                    txt_add005.Text = "";
+                    update_total();
+                }
+                catch { }
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                txt_add010.Focus();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt_add1.Focus();
+            XmlDocument data = new XmlDocument();
+            string path = @"coins.xml";
+            data.Load(path);
+            qt1 = Convert.ToInt32(data.SelectSingleNode("/coins/r1").Attributes["quant"].Value);
+            txt_qt1.Text = qt1.ToString();
+            txt_t1.Text = "R$ " + String.Format("{0:0.00}", Math.Round((qt1*1.0), 2));
+
+            qt050 = Convert.ToInt32(data.SelectSingleNode("/coins/r050").Attributes["quant"].Value);
+            txt_qt050.Text = qt050.ToString();
+            txt_t050.Text = "R$ " + String.Format("{0:0.00}", Math.Round((qt050 * 0.5), 2));
+
+            qt025 = Convert.ToInt32(data.SelectSingleNode("/coins/r025").Attributes["quant"].Value);
+            txt_qt025.Text = qt025.ToString();
+            txt_t025.Text = "R$ " + String.Format("{0:0.00}", Math.Round((qt025 * 0.25), 2));
+
+            qt010 = Convert.ToInt32(data.SelectSingleNode("/coins/r010").Attributes["quant"].Value);
+            txt_qt010.Text = qt010.ToString();
+            txt_t010.Text = "R$ " + String.Format("{0:0.00}", Math.Round((qt010 * 0.1), 2));
+
+            qt005 = Convert.ToInt32(data.SelectSingleNode("/coins/r005").Attributes["quant"].Value);
+            txt_qt005.Text = qt005.ToString();
+            txt_t005.Text = "R$ " + String.Format("{0:0.00}", Math.Round((qt005 * 0.05), 2));
+            update_total();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void cmd_save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                XmlDocument data = new XmlDocument();
+                XmlNode docNode = data.CreateXmlDeclaration("1.0", "UTF-8", null);
+                data.AppendChild(docNode);
+                XmlElement coin_Elem = data.CreateElement("coins");
+                data.AppendChild(coin_Elem);
+                XmlNode coin1Node = data.CreateElement("r1");
+                XmlNode coin050Node = data.CreateElement("r050");
+                XmlNode coin025Node = data.CreateElement("r025");
+                XmlNode coin010Node = data.CreateElement("r010");
+                XmlNode coin005Node = data.CreateElement("r005");
+
+                coin_Elem.AppendChild(coin1Node);
+                XmlAttribute save1 = data.CreateAttribute("quant");
+                save1.InnerText = txt_qt1.Text;
+                coin1Node.Attributes.Append(save1);
+
+                coin_Elem.AppendChild(coin050Node);
+                XmlAttribute save050 = data.CreateAttribute("quant");
+                save050.InnerText = txt_qt050.Text;
+                coin050Node.Attributes.Append(save050);
+
+                coin_Elem.AppendChild(coin025Node);
+                XmlAttribute save025 = data.CreateAttribute("quant");
+                save025.InnerText = txt_qt025.Text;
+                coin025Node.Attributes.Append(save025);
+
+                coin_Elem.AppendChild(coin010Node);
+                XmlAttribute save010 = data.CreateAttribute("quant");
+                save010.InnerText = txt_qt010.Text;
+                coin010Node.Attributes.Append(save010);
+
+                coin_Elem.AppendChild(coin005Node);
+                XmlAttribute save005 = data.CreateAttribute("quant");
+                save005.InnerText = txt_qt005.Text;
+                coin005Node.Attributes.Append(save005);
+
+                data.Save(@"coins.xml");
+            }
+            catch { }
+        }
+    }
+}
